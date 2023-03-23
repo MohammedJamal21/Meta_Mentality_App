b9ea9f39a6a0b1faa24d8c27b6c10f36937338a7
MohammedJamal21 <mojamal.it@gmail.com>
2023-03-23 18:28:23 +0100

    Created StartCourseScreen

---
+++ lib/views/courses/start_course_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StartCourseScreen extends StatelessWidget {
+  const StartCourseScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
