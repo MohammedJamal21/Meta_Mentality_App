c49ae693b868421a5850e6bf16f9ee07858ac5cf
MohammedJamal21 <mojamal.it@gmail.com>
2023-04-10 02:36:32 +0200

    Added VideoPlayer

--- lib/views/courses/module_screen.dart
+++ lib/views/courses/module_screen.dart
@@ -1,10 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class ModuleScreen extends StatelessWidget {
+class ModuleScreen extends StatefulWidget {
   const ModuleScreen({Key? key}) : super(key: key);
 
   @override
+  State<ModuleScreen> createState() => _ModuleScreenState();
+}
+
+class _ModuleScreenState extends State<ModuleScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.play();
+    _controller.setLooping(true);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // If the video is paused, play it.
+              _controller.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leadingWidth: 100,
+        leading: TextButton.icon(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            size: 16,
+            color: Colors.black,
+          ),
+          label: const Text(
+            'Back',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //----- COLUMN
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'Header for the first module',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: const [
+                  Text('PART 1 OF 12'),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                      child: Divider(
+                    thickness: 2,
+                    color: Colors.black,
+                  )),
+                ],
+              ),
+            ),
+            FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the VideoPlayerController has finished initialization, use
+                  // the data it provides to limit the aspect ratio of the video.
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        // Use the VideoPlayer widget to display the video.
+                        child: VideoPlayer(_controller),
+                      ),
+                    ),
+                  );
+                } else {
+                  // If the VideoPlayerController is still initializing, show a
+                  // loading spinner.
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            )
+
+            //-------------- END OF COLUMN
+          ],
+        ),
+      ),
+    );
   }
 }
